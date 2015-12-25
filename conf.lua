15702b8cbbc486f35f7eccb4f32033fa5fdf2bf4
Alex <alexparson1@gmail.com>
2015-12-25 18:13:20 -0500

    testing for web

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
 t.console =true
-t.window.width = 1000                -- The window width (number)
-t.window.height = 700  
+--t.window.width = 1000                -- The window width (number)
+--t.window.height = 700  
 end
